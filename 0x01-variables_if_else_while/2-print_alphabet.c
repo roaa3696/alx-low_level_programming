876773231dd5d87e0b6194089207c349f440189c
roaa <roaaayman234@gmail.com>
2023-09-10 07:16:27 +0300

    prints the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// prints the alphabets
+
+int main (void)
+{
+    char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+
+    for (i = 0; i <26; i++)
+    {
+        putchar(alphabets[i]);
+    }
+    putchar('\n');
+    return (0);
+}
