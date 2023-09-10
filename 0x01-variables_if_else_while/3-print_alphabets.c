7a82606b8d45c9b80854ee11b46d0c95ad0bc33c
roaa <roaaayman234@gmail.com>
2023-09-10 07:45:45 +0300

    fixing errors

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main (void)
 {
 char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int i;
-    for (i = 0; i > 52; i++)
+    for (i = 0; i < 52; i++)
     {
         putchar(alpha[i]);
     
