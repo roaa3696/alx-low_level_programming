2ffe59b646db418881b4d5f4fa280ec193cc5089
roaa <roaaayman234@gmail.com>
2023-09-10 11:31:59 +0300

    the lowwer alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print lowwer case alphabet reversed
+ * return = 0
+ */
+
+int main (void)
+{
+     char c;
+     for (c = 'z'; c >= 'a', c--;)
+     {
+         putchar('c');
+     }
+     putchar('\n');
+     return (0);
+}
