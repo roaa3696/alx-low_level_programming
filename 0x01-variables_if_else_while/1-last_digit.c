0ffa65d33b0226cee2772d55d04b44ce55152243
roaa <roaaayman234@gmail.com>
2023-09-10 06:59:12 +0300

    generating a random numder and determining it's value kinda

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * prints the last digit of a random no,
+ *  and determines whether it's greater than 5, less than 6 or zero 
+*/
+
+int main(void)
+{
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if ((n% 10) >5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+    }
+    else if ((n % 10) < 6 && (n % 10) != 0)
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+    }
+    else
+    {
+        printf("Last digit of %d is %d and is 0\n", n, n % 10);
+    }
+    return (0);
+}
