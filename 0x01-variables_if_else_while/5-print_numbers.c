05e43b7e5d418f51a3948b6b6de737496b1d8a21
roaa <roaaayman234@gmail.com>
2023-09-10 08:59:26 +0300

    listing numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * print numbers from 0 - 9
+ * return = 0
+*/
+
+int main(void)
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        printf(%d, i);
+    }
+    putchar('n');
+    return (0);
+}
