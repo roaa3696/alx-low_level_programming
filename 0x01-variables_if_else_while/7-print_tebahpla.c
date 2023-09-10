a2b7a1b7ec9f6bef4b03c804cc37186d272798a4
roaa <roaaayman234@gmail.com>
2023-09-10 13:03:20 +0300

    fixing number 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,9 +8,9 @@
 int main (void)
 {
      char c;
-     for (c = 'z'; c >= 'a', c = c - 1 )
+     for (c = 'z'; c >= 'a'; c--)
      {
-         putchar('c');
+         putchar(c);
      }
      putchar('\n');
      return (0);
