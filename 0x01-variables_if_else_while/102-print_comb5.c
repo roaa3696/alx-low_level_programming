a0e1524cc9a1ca06ea26fed9e821336a7c1dcdae
roaa <roaaayman234@gmail.com>
2023-09-11 14:48:59 +0300

    betty style

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,29 +6,25 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
+int main(void) {
     int i, j;
 
-    for (i = 0; i < 100; i++)
+    for (i = 0; i <= 99; i++) 
     {
-        for (j = 0; j < 100; j++)
+        for (j = i; j <= 99; j++) 
         {
-            if (i < j)
+            putchar((i / 10) + 0);
+            putchar((i % 10) + 0);
+            putchar(' ');
+            putchar((j / 10) + 0);
+            putchar((j % 10) + 0);
+            if (i != 99 || j != 99) 
             {
-                putchar((i / 10) + 48);
-                putchar((i % 10) + 48);
+                putchar(',');
                 putchar(' ');
-                putchar((j / 10) + 48);
-                putchar((j % 10) + 48);
-                if (i != 99 || j != 99)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
             }
         }
     }
-    putchar('\n');
-    return (0);
+
+    return 0;
 }
