170b213aea38cb5b5773423f5599cd574d6b0746
roaa <roaaayman234@gmail.com>
2023-09-13 03:19:14 +0300

    fixing 16 errors

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,10 +27,10 @@ int main(void)
                 bef = aft - bef;
         }
 
-        bef1 = (bef / 1); 
-        bef2 = (bef % 1);
-        aft1 = (aft / 1); 
-        aft2 = (aft % 1);
+        bef1 = (bef / l); 
+        bef2 = (bef % l);
+        aft1 = (aft / l); 
+        aft2 = (aft % l);
 
         for (i = 1; i <91; i++)
         {
@@ -39,15 +39,15 @@ int main(void)
                 bef=aft - bef;
         }
 
-        bef1 = (bef / 1); 
-        bef2 = (bef % 1);
-        aft1 = (aft / 1); 
-        aft2 = (aft % 1);
+        bef1 = (bef / l); 
+        bef2 = (bef % l);
+        aft1 = (aft / l); 
+        aft2 = (aft % l);
 
         for (i = 92; i < 99; ++i)
         {
-            printf(", %lu", aft1 + (aft2 / 1)); 
-            printf("%lu", aft2 % 1); 
+            printf(", %lu", aft1 + (aft2 / l)); 
+            printf("%lu", aft2 % l); 
             aft1 = aft1 + bef1;
             bef1 = aft1 - bef1;
             aft2 = aft2 + bef2; 
