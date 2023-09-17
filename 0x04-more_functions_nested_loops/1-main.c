3204a665674dbaf3986b6216dcb74a2358c8ba3a
roaa <roaaayman234@gmail.com>
2023-09-17 15:44:57 +0300

    no 1

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -6,12 +6,13 @@
  *
  * Return: Always 0.
  */
+int _isdigit(int);
+
 int main(void)
 {
     char c;
 
     c = '0';
-    _isdigit(c);
     printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
     printf("%c: %d\n", c, _isdigit(c));
