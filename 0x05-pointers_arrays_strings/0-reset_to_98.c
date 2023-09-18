c9e9788a55fce4f575976264d0bc7b02c246b1ec
roaa <roaaayman234@gmail.com>
2023-09-18 20:50:02 +0300

    del trailing white space

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an parameter
  * and updates the value it points to to 98
- * @n: the pointer used to change the value 
+ * @n: the pointer used to change the value
  * On: input
  * Return: n
  */
