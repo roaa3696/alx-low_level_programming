20e1986e7aff2006cab7505e1de5eebb886703f0
roaa <roaaayman234@gmail.com>
2023-09-18 21:37:00 +0300

    no4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_rev - imprime an reversa
- * @s: string 
+ * @s: string
  * return: 0
- */ 
+ */
 void print_rev(char *s)
 {
-int longi = 0; 
+int longi = 0;
 int o;
 
 while (*s != '\0')
@@ -15,7 +15,7 @@ longi++;
 s++;
 }
 s--;
-for (o= longi; o > 0; o--)
+for (o = longi; o > 0; o--)
 {
 _putchar(*s);
 s--;
