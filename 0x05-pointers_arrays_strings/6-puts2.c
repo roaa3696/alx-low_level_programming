805abb022bd3f7d9dc51970cb53af49e83b6aea0
roaa <roaaayman234@gmail.com>
2023-09-19 05:22:40 +0300

    trailing white space

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -18,7 +18,7 @@ y++;
 longi++;
 }
 t = longi - 1;
-for (o  =0; o <= t; o++)
+for (o = 0; o <= t; o++)
 {
 if (o % 2 == 0)
 {
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_half - a function that prints half of a string 
+ * puts_half - a function that prints half of a string
  * if odd len, n = (length_of_the_string- 1) / 2
  * @str: input
  * Return: half of input
@@ -10,9 +10,9 @@ void puts_half(char *str)
 {
 int a, n, longi;
 
-longi = 0; 
+longi = 0;
 
-for (a = 0; str[a] != '\0'; a++) 
+for (a = 0; str[a] != '\0'; a++)
 longi++;
 
 n = (longi / 2);
