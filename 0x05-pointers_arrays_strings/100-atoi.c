028be38d88acfb07c55582a73e49f3a4afa28a93
roaa <roaaayman234@gmail.com>
2023-09-19 05:58:29 +0300

    no9

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,13 +18,15 @@ digit = 0;
 
 while (s[len] != '\0')
 len++;
+
 while (i < len && f == 0)
 {
+if (s[i] != '-')
 ++d;
 
 if (s[i] >= '0' && s[i] <= '9')
 {
-digit = s[i] = '0';
+digit = s[i] - '0';
 if (d % 2)
 digit = -digit;
 n = n * 10 + digit;
